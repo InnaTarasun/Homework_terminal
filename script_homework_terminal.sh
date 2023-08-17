ffa5f2bfe16f0de446d6a2a7d272d106a544422e
Тарасун Инна Владимировна <tarasun@sc.softclub.by>
2023-08-17 14:30:10 +0200

    new script

---
+++ script_homework_terminal.sh
@@ -0,0 +1,9 @@
+mkdir Inna1
+cd Inna1
+mkdir Folder1 Folder2 Folder3
+cd Folder1
+touch Letter1.txt Letter2.txt Letter3.txt Pets.json Members.json
+mkdir Catalog1 Catalog2 Catalog3
+ls
+mv Letter2.txt ../Folder2/Letter2.txt
+mv Pets.json ../Folder2/Letter2.txt
